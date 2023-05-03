33923e9edce94ae93dd8c7f9a56e2fabc169f9a6
g0sie <malgorzataderkacz3@gmail.com>
2023-05-03 20:05:13 +0200

    feat: color 'Welcome' on WelcomePage

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -3,7 +3,15 @@ import * as React from 'react';
 export const WelcomePage: React.FunctionComponent = () => {
   return (
     <>
-      <h1>Welcome page</h1>
+      <h1>
+        <span style={{ color: 'var(--purple-300)' }}>W</span>
+        <span style={{ color: 'var(--cyan-300)' }}>e</span>
+        <span style={{ color: 'var(--orange-400)' }}>l</span>
+        <span style={{ color: 'var(--green-300)' }}>c</span>
+        <span style={{ color: 'var(--red-400)' }}>o</span>
+        <span style={{ color: 'var(--blue-500)' }}>m</span>
+        <span style={{ color: 'var(--orange-500)' }}>e</span> page
+      </h1>
     </>
   );
 };
