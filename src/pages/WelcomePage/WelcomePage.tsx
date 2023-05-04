d236f74e33e0d7dd9db6186d5bb2511754a5b6ba
g0sie <malgorzataderkacz3@gmail.com>
2023-05-04 13:33:55 +0200

    fix: use SCSS variables instead of CSS variables

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 
+import './WelcomePage.scss';
+
 export const WelcomePage: React.FunctionComponent = () => {
   return (
     <>
       <h1>
-        <span style={{ color: 'var(--purple-300)' }}>W</span>
-        <span style={{ color: 'var(--cyan-300)' }}>e</span>
-        <span style={{ color: 'var(--orange-400)' }}>l</span>
-        <span style={{ color: 'var(--green-300)' }}>c</span>
-        <span style={{ color: 'var(--red-400)' }}>o</span>
-        <span style={{ color: 'var(--blue-500)' }}>m</span>
-        <span style={{ color: 'var(--orange-500)' }}>e</span> page
+        <span className="w">W</span>
+        <span className="e">e</span>
+        <span className="l">l</span>
+        <span className="c">c</span>
+        <span className="o">o</span>
+        <span className="m">m</span>
+        <span className="e">e</span> page
       </h1>
     </>
   );
