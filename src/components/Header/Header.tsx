c322b05ed56824ca430342c01c7f77fc996b0956
g0sie <malgorzataderkacz3@gmail.com>
2023-05-05 18:10:57 +0200

    refactor: create a component for header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.scss';
+
+const Header: React.FunctionComponent = () => {
+  return (
+    <>
+      <header className="header"></header>
+    </>
+  );
+};
+
+export default Header;
