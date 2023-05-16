a4aef32c715d6f4c477521b14df83fab53abc0f8
g0sie <malgorzataderkacz3@gmail.com>
2023-05-16 17:37:33 +0200

    feat: add 'Sign out' button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 
 import Logo from '../Logo/Logo';
@@ -7,6 +8,41 @@ import Logo from '../Logo/Logo';
 import './Header.scss';
 
 const Header: React.FunctionComponent = () => {
+  // isAuthenticated state is only for now, since we don't have authentication yet
+  // i'll change that later
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const navigate = useNavigate();
+
+  const signIn = () => setIsAuthenticated(true);
+  const signOut = () => {
+    setIsAuthenticated(false);
+    navigate('/');
+  };
+
+  const buttonsForAuthenticated = (
+    <>
+      <Button onClick={signOut} color="secondary" variant="contained">
+        Sign out
+      </Button>
+    </>
+  );
+
+  const buttonsForUnauthenticated = (
+    <>
+      <Link to="/sign-in">
+        <Button onClick={signIn} color="secondary" variant="contained">
+          Sign in
+        </Button>
+      </Link>
+
+      <Link to="/sign-up">
+        <Button onClick={signIn} color="secondary" variant="contained">
+          Sign up
+        </Button>
+      </Link>
+    </>
+  );
+
   return (
     <header className="header">
       <nav className="header__navbar">
@@ -15,17 +51,7 @@ const Header: React.FunctionComponent = () => {
         </Link>
 
         <div className="navbar__right">
-          <Link to="/sign-in">
-            <Button color="secondary" variant="contained">
-              Sign in
-            </Button>
-          </Link>
-
-          <Link to="/sign-up">
-            <Button color="secondary" variant="contained">
-              Sign up
-            </Button>
-          </Link>
+          {isAuthenticated ? buttonsForAuthenticated : buttonsForUnauthenticated}
         </div>
       </nav>
     </header>
