2680db01a966afbb354f967dab01ddaf88faf7a4
g0sie <malgorzataderkacz3@gmail.com>
2023-05-16 22:01:00 +0200

    feat: add TeamSection

---
+++ src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './TeamSection.scss';
+
+const TeamSection: React.FunctionComponent = () => {
+  return (
+    <>
+      <section className="team-section"></section>
+    </>
+  );
+};
+
+export default TeamSection;
