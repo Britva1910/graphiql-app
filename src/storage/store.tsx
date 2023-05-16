9e83deeb8e55f48e010c424f10c46c3ab0cfc73a
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2023-05-16 19:48:52 +0300

    feat: implement base redux for user

--- src/storage/store.tsx
+++ src/storage/store.tsx
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import editorReducer from './EditorFieldSlice';
 import responseReducer from './ResponseFieldSlice';
+import useReducer from './UserSlice';
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
@@ -9,6 +10,7 @@ const store = configureStore({
   reducer: {
     editor: editorReducer,
     response: responseReducer,
+    user: useReducer,
   },
 });
 export default store;
