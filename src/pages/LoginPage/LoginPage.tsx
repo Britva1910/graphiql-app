f9a3e755257a260943efd217236aa1ac0da23849
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2023-05-16 21:40:51 +0300

    feat: add simple form for tests and integrate firebase

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { AuthForm } from '../../components/AuthForm/AuthForm';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const handleLogin = (email: string, password: string): void => {
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <>
+      <AuthForm handleForm={handleLogin} formTitle="Login" />
+    </>
+  );
 };
 
 export { LoginPage };
