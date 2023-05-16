84e7abfdf771190adb5a355c45b4edb64d4d294d
g0sie <malgorzataderkacz3@gmail.com>
2023-05-16 16:53:43 +0200

    feat: add 'Sign in' and 'Sign up' buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 import Logo from '../Logo/Logo';
 
@@ -9,9 +10,23 @@ const Header: React.FunctionComponent = () => {
   return (
     <header className="header">
       <nav className="header__navbar">
-        <Link to="/" className="navbar__link">
+        <Link to="/" style={{ textDecoration: 'none' }}>
           <Logo />
         </Link>
+
+        <div className="navbar__right">
+          <Link to="/">
+            <Button color="secondary" variant="contained">
+              Sign in
+            </Button>
+          </Link>
+
+          <Link to="/">
+            <Button color="secondary" variant="contained">
+              Sign up
+            </Button>
+          </Link>
+        </div>
       </nav>
     </header>
   );
