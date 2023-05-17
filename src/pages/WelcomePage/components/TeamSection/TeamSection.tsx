7e5b29ee521306ea62ce8dcff0816fbd78130a46
g0sie <malgorzataderkacz3@gmail.com>
2023-05-17 15:59:40 +0200

    feat: add DeveloperCard

--- src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
+++ src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import DeveloperCard from '../DeveloperCard/DeveloperCard';
+
 import './TeamSection.scss';
 
 const TeamSection: React.FunctionComponent = () => {
@@ -7,6 +9,7 @@ const TeamSection: React.FunctionComponent = () => {
     <section className="team-section">
       <div className="section__inner">
         <h2 className="team-section__heading">Meet our team!</h2>
+        <DeveloperCard firstName="Małgorzata" lastName="Derkacz" githubUsername="g0sie" />
       </div>
     </section>
   );
