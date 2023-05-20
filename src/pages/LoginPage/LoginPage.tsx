eb9e9db77d905bd695b630597e870acf34482d28
Ivan Lashuk <85491523+Britva1910@users.noreply.github.com>
2023-05-20 10:31:13 +0300

    feat: implement register page with form

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { AuthForm } from '../../components/AuthForm/AuthForm';
 
 const LoginPage = () => {
@@ -7,17 +7,57 @@ const LoginPage = () => {
     const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
+        console.log(auth);
       })
       .catch((error) => {
         console.log(error.message);
       });
   };
 
+  const getToken = async () => {
+    const auth = getAuth();
+    const userToken = auth.currentUser?.getIdToken();
+    console.log(userToken);
+  };
+
+  const chackUser = () => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    console.log(user);
+  };
+
+  const singOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log('sing out successful');
+      })
+      .catch(() => {
+        // An error happened.
+      });
+  };
+
+  //event subs
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log('User loged', uid);
+    } else {
+      console.log('User NOT auth');
+    }
+  });
+
   return (
     <>
       <AuthForm handleForm={handleLogin} formTitle="Login" />
+
+      <button onClick={getToken}>Get token</button>
+
+      <button onClick={chackUser}>Chack user</button>
+
+      <button onClick={singOut}>Sing out</button>
     </>
   );
 };
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { AuthForm } from '../../components/AuthForm/AuthForm';
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const handleSingUp = (email: string, password: string): void => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        // ...
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  return (
+    <>
+      <AuthForm handleForm={handleSingUp} formTitle="Sing up" />
+    </>
+  );
 };
 
 export { RegisterPage };
