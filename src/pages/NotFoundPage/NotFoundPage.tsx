2ab5c042dd774f8381107118a5dca9e7a7cc3892
g0sie <malgorzataderkacz3@gmail.com>
2023-05-25 21:52:41 +0200

    fix: remove useless fragment

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -7,22 +7,21 @@ import './NotFoundPage.scss';
 
 const NotFoundPage: React.FunctionComponent = () => {
   return (
-    <>
-      <div className="not-found-page">
-        <img
-          className="sad-miku-gif"
-          src="https://media.tenor.com/5--oAdlt0OwAAAAd/sad-crying.gif"
-          alt="sad Hatsune Miku"
-        />
-        <p className="sad-miku-text">
-          it makes me sad when you try to access a page that doesn&apos;t exist
-        </p>
+    <div className="not-found-page">
+      <img
+        className="sad-miku-gif"
+        src="https://media.tenor.com/5--oAdlt0OwAAAAd/sad-crying.gif"
+        alt="sad Hatsune Miku"
+      />
 
-        <Link to="/">
-          <Button color="red">Go away</Button>
-        </Link>
-      </div>
-    </>
+      <p className="sad-miku-text">
+        it makes me sad when you try to access a page that doesn&apos;t exist
+      </p>
+
+      <Link to="/">
+        <Button color="red">Go away</Button>
+      </Link>
+    </div>
   );
 };
 
