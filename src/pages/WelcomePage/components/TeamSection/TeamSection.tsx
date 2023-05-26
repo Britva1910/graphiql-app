6d862e6e03dabb1b75a3d00fdb53e4ce4774f528
g0sie <malgorzataderkacz3@gmail.com>
2023-05-26 12:36:32 +0200

    feat: add more developers

--- src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
+++ src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
@@ -9,7 +9,11 @@ const TeamSection: React.FunctionComponent = () => {
     <section className="team-section">
       <div className="section__inner">
         <h2 className="team-section__heading">Meet our team!</h2>
-        <DeveloperCard firstName="Małgorzata" lastName="Derkacz" githubUsername="g0sie" />
+        <div className="team-section__developers">
+          <DeveloperCard firstName="Ivan" lastName="Lashuk" githubUsername="britva1910" />
+          <DeveloperCard firstName="Ilya" lastName="Hrushevich" githubUsername="gru6" />
+          <DeveloperCard firstName="Małgorzata" lastName="Derkacz" githubUsername="g0sie" />
+        </div>
       </div>
     </section>
   );
