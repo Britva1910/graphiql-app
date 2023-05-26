a569a27a8d4dec85e171237cb6a13d7a610707ff
g0sie <malgorzataderkacz3@gmail.com>
2023-05-26 13:09:24 +0200

    feat: make DeveloperCard draggable

--- src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
+++ src/pages/WelcomePage/components/TeamSection/TeamSection.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import DeveloperCard from '../DeveloperCard/DeveloperCard';
 
 import './TeamSection.scss';
 
 const TeamSection: React.FunctionComponent = () => {
+  const teamSectionRef = useRef(null);
+
   return (
-    <section className="team-section">
+    <section className="team-section" ref={teamSectionRef}>
       <div className="section__inner">
         <h2 className="team-section__heading">Meet our team!</h2>
         <div className="team-section__developers">
-          <DeveloperCard firstName="Ivan" lastName="Lashuk" githubUsername="britva1910" />
-          <DeveloperCard firstName="Ilya" lastName="Hrushevich" githubUsername="gru6" />
-          <DeveloperCard firstName="Małgorzata" lastName="Derkacz" githubUsername="g0sie" />
+          <DeveloperCard
+            firstName="Ivan"
+            lastName="Lashuk"
+            githubUsername="britva1910"
+            dragConstraints={teamSectionRef}
+          />
+          <DeveloperCard
+            firstName="Ilya"
+            lastName="Hrushevich"
+            githubUsername="gru6"
+            dragConstraints={teamSectionRef}
+          />
+          <DeveloperCard
+            firstName="Małgorzata"
+            lastName="Derkacz"
+            githubUsername="g0sie"
+            dragConstraints={teamSectionRef}
+          />
         </div>
       </div>
     </section>
