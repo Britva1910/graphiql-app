3d7b642045160033e6bc1d33354bf339979d6551
g0sie <malgorzataderkacz3@gmail.com>
2023-05-27 19:57:10 +0200

    fix: add target="_blank" to footer links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,20 +10,40 @@ const Footer: React.FC = () => {
     <footer className="footer">
       <div className="footer__inner">
         <div className="footer__authors">
-          <a className="github-link" href="https://github.com/britva1910">
+          <a
+            className="github-link"
+            href="https://github.com/britva1910"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GitHubIcon /> Britva1910
           </a>
-          <a className="github-link" href="https://github.com/gru6">
+          <a
+            className="github-link"
+            href="https://github.com/gru6"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GitHubIcon /> gru6
           </a>
-          <a className="github-link" href="https://github.com/g0sie">
+          <a
+            className="github-link"
+            href="https://github.com/g0sie"
+            target="_blank"
+            rel="noreferrer"
+          >
             <GitHubIcon /> g0sie
           </a>
         </div>
 
         <span className="footer__year">&copy; 2023</span>
 
-        <a className="footer__rs-logo" href="https://rs.school/react/">
+        <a
+          className="footer__rs-logo"
+          href="https://rs.school/react/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <RSLogo height="1.2rem" />
         </a>
       </div>
