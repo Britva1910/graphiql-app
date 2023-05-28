5008c2f46d6183afa7fdf41ad1f8fdf43c06778e
g0sie <malgorzataderkacz3@gmail.com>
2023-05-28 15:34:14 +0200

    feat: style main page

--- src/pages/MainPage/components/HeadersField.tsx
+++ src/pages/MainPage/components/HeadersField.tsx
@@ -1,32 +1,30 @@
-import * as React from 'react';
-
+import React from 'react';
 import AceEditor from 'react-ace';
+
 import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/theme-solarized_dark';
+import 'ace-builds/src-noconflict/theme-cobalt';
 
 export const Headers: React.FunctionComponent = () => {
   return (
-    <>
-      <div style={{ width: '100%' }}>
-        <AceEditor
-          mode="javascript"
-          theme="solarized_dark"
-          name="my-text-headers"
-          value="Headers (optional)"
-          width="100%"
-          fontSize={14}
-          showPrintMargin={true}
-          showGutter={true}
-          highlightActiveLine={true}
-          editorProps={{ $blockScrolling: Infinity }}
-          style={{ height: '100%' }}
-          wrapEnabled={true}
-          setOptions={{
-            showLineNumbers: true,
-            tabSize: 2,
-          }}
-        />
-      </div>
-    </>
+    <div style={{ width: '100%' }}>
+      <AceEditor
+        mode="javascript"
+        theme="cobalt"
+        name="my-text-headers"
+        value="Headers (optional)"
+        width="100%"
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        editorProps={{ $blockScrolling: Infinity }}
+        style={{ height: '100%' }}
+        wrapEnabled={true}
+        setOptions={{
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 };
--- src/pages/MainPage/components/ResponseField.tsx
+++ src/pages/MainPage/components/ResponseField.tsx
@@ -1,10 +1,9 @@
-import * as React from 'react';
-
 import AceEditor from 'react-ace';
+import { useSelector } from 'react-redux';
+
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-solarized_dark';
 
-import { useSelector } from 'react-redux';
 import { RootState } from '../../../storage/store';
 
 export const Response = () => {
@@ -12,28 +11,26 @@ export const Response = () => {
   const responseError = useSelector((state: RootState) => state.response.error);
 
   return (
-    <>
-      <div style={{ display: 'flex', height: '100%' }}>
-        <AceEditor
-          mode="javascript"
-          theme="solarized_dark"
-          readOnly={true}
-          name="my-text-response"
-          value={responseValue ? responseValue : responseError}
-          width="100%"
-          fontSize={14}
-          showPrintMargin={true}
-          showGutter={true}
-          highlightActiveLine={true}
-          editorProps={{ $blockScrolling: Infinity }}
-          style={{ height: '100%' }}
-          wrapEnabled={true}
-          setOptions={{
-            showLineNumbers: true,
-            tabSize: 2,
-          }}
-        />
-      </div>
-    </>
+    <div style={{ display: 'flex', height: '100%' }}>
+      <AceEditor
+        mode="javascript"
+        theme="cobalt"
+        readOnly={true}
+        name="my-text-response"
+        value={responseValue ? responseValue : responseError}
+        width="100%"
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        editorProps={{ $blockScrolling: Infinity }}
+        style={{ height: '100%' }}
+        wrapEnabled={true}
+        setOptions={{
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 };
